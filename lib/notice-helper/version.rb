388f524f4f93356d23fe708bd4418f2a95e2ba75
Paul Cook <paul.cook@alternateversion.com>
2012-09-24 15:16:00 -0400

    updated to use bootstrap alert classes

--- lib/notice-helper/version.rb
+++ lib/notice-helper/version.rb
@@ -1,7 +1,7 @@
 module Ogg
   module View
     module Helpers
-      VERSION = "1.0.0"
+      VERSION = "2.0.0"
     end
   end
 end
--- lib/notice-helper/view_helper.rb
+++ lib/notice-helper/view_helper.rb
@@ -5,9 +5,9 @@ module NoticeHelper
     def handle_notices(ignore=nil)
       flsh = ''
       flash.each do |key,msg|
-        flsh << '<div class="flash '+key.to_s+'"><div class="close_notice">Close</div>'+simple_format(msg)+'</div>' unless ignore and ignore.include?(key)
+        flsh << '<div class="alert alert-block alert-'+key.to_s+'"><a class="close" data-dismiss="alert" href="#">&times;</a>'+simple_format(msg)+'</div>' unless ignore and ignore.include?(key)
       end
-      flsh << '<div class="flash" style="display:none"><div class="close_notice">Close</div></div>'
+      flsh << '<div class="alert"><a class="close" data-dismiss="alert" href="#">&times;</a></div>'
       flsh.html_safe
     end
   end
